8d4af8fabec33dda128903364b4c4470a1da87c0
Rodrigo Lombardo <rodrigogeribola@hotmail.com>
2022-09-15 13:47:07 -0300

    use swagger in any enviroments

--- 01 - Presentation/Project.SchoolDepartment.Web.API/Program.cs
+++ 01 - Presentation/Project.SchoolDepartment.Web.API/Program.cs
@@ -18,12 +18,8 @@ builder.Services.ConfigureServices(builder.Configuration);
 
 WebApplication app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseMiddleware(typeof(ErrorMiddleware));
 
