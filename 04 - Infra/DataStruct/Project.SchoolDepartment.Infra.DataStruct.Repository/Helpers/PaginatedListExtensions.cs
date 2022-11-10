136221538b8f5093d7f4033a5a19047ba795187f
Rodrigo Geribola <rodrigogeribola@hotmail.com>
2022-11-10 09:06:41 -0300

    new Extension Method

---
+++ 04 - Infra/DataStruct/Project.SchoolDepartment.Infra.DataStruct.Repository/Helpers/PaginatedListExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Project.SchoolDepartment.Infra.DataStruct.Repository.Helpers;
+
+public static class PaginatedListExtensions
+{
+	public static PaginatedList<TSource> ToPaginatedList<TSource>(this IEnumerable<TSource> source, int page, int quantity)
+	{
+		return PaginatedList<TSource>.CreateInstance(source, page, quantity);
+	}
+
+	public static async Task<PaginatedList<TSource>> ToPaginatedListAsync<TSource>(this IEnumerable<TSource> source, int page, int quantity)
+	{
+		return await PaginatedList<TSource>.CreateInstanceAsync(source, page, quantity);
+	}
+}
