82ba148713ef927a59a660e792bd92dc87d40fba
Rodrigo Geribola <rodrigogeribola@hotmail.com>
2022-11-23 15:30:02 -0300

    add new logic to AutoMapper

---
+++ 04 - Infra/CrossCutting/Project.SchoolDepartment.Infra.CrossCutting.CCT/AutoMapper/MapProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace Project.SchoolDepartment.Infra.CrossCutting.CCT.AutoMapper;
+
+public sealed class MapProfile : Profile
+{
+	public new IMappingExpression CreateMap(Type sourceType, Type destinationType)
+	{
+		return base.CreateMap(sourceType, destinationType);
+	}
+
+	public new IMappingExpression<TSource, TDestination> CreateMap<TSource, TDestination>()
+	{
+		return base.CreateMap<TSource, TDestination>();
+	}
+}
--- 04 - Infra/CrossCutting/Project.SchoolDepartment.Infra.CrossCutting.CCT/ConfigureServiceCollection.cs
+++ 04 - Infra/CrossCutting/Project.SchoolDepartment.Infra.CrossCutting.CCT/ConfigureServiceCollection.cs
@@ -1,11 +1,16 @@
-﻿using Hangfire;
+﻿using AutoMapper;
+using Hangfire;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Project.SchoolDepartment.Application.DTOs;
 using Project.SchoolDepartment.Application.Interfaces;
 using Project.SchoolDepartment.Application.Services;
 using Project.SchoolDepartment.Domain.Interfaces;
+using Project.SchoolDepartment.Domain.Models;
 using Project.SchoolDepartment.Domain.Services;
+using Project.SchoolDepartment.Infra.CrossCutting.CCT.AutoMapper;
 using Project.SchoolDepartment.Infra.DataStruct.Data.Contexts;
+using Project.SchoolDepartment.Infra.DataStruct.Data.Entities;
 using Project.SchoolDepartment.Infra.DataStruct.Repository.Interfaces;
 using Project.SchoolDepartment.Infra.DataStruct.Repository.Repositories;
 
@@ -23,7 +28,43 @@ public static class ConfigureServiceCollection
 		services.AddScoped(typeof(IDomainAlunoService), typeof(DomainAlunoService));
 		services.AddScoped(typeof(IAlunoRepository), typeof(AlunoRepository));
 
+		services.ConfigureMaps();
+
 		services.ConfigureHangfire(configuration);
+
+		return services;
+	}
+
+	private static IServiceCollection ConfigureMaps(this IServiceCollection services)
+	{
+		var profile = new MapProfile();
+
+		#region TO DTO
+
+		profile.CreateMap(typeof(AlunoModel), typeof(AlunoDTO));
+		profile.CreateMap(typeof(CursoModel), typeof(CursoDTO));
+		profile.CreateMap(typeof(TurmaModel), typeof(TurmaDTO));
+		profile.CreateMap(typeof(TelefoneModel), typeof(TelefoneDTO));
+
+		#region PAGINATED'S
+
+		profile.CreateMap(typeof(PaginatedModel<AlunoModel>), typeof(PaginatedDTO<AlunoDTO>));
+		profile.CreateMap(typeof(PaginatedModel<CursoModel>), typeof(PaginatedDTO<CursoDTO>));
+		profile.CreateMap(typeof(PaginatedModel<TurmaModel>), typeof(PaginatedDTO<TurmaDTO>));
+		profile.CreateMap(typeof(PaginatedModel<TelefoneModel>), typeof(PaginatedDTO<TelefoneDTO>));
+
+		#endregion
+
+		#endregion
+
+		#region TO MODEL
+
+		profile.CreateMap(typeof(Aluno), typeof(AlunoModel));
+		profile.CreateMap(typeof(Curso), typeof(CursoModel));
+		profile.CreateMap(typeof(Turma), typeof(TurmaModel));
+		profile.CreateMap(typeof(Telefone), typeof(TelefoneModel));
+
+		#endregion
 
 		return services;
 	}
