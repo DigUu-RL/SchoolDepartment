3bdc8f61bc5c647db2e161e739cd9625184f839c
Rodrigo Geribola <rodrigogeribola@hotmail.com>
2022-12-15 17:00:01 -0300

    add email validation

--- 03 - Domain/Project.SchoolDepartment.Domain/Helpers/Util.cs
+++ 03 - Domain/Project.SchoolDepartment.Domain/Helpers/Util.cs
@@ -5,8 +5,31 @@ namespace Project.SchoolDepartment.Domain.Helpers;
 
 public static class Util
 {
+	private static readonly Random _random = new();
+
 	public static async Task<string> CreateStringHash(string content)
 	{
 		return Convert.ToBase64String(await SHA512.Create().ComputeHashAsync(new MemoryStream(Encoding.UTF8.GetBytes(content))));
+	}
+
+	public static string RandomString(int length)
+	{
+		char[] chars = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz" + "1234567890").ToArray();
+
+		var builder = new StringBuilder();
+
+		for (int i = 0; i < length; i++)
+		{
+			int x = _random.Next(chars.Length);
+			builder.Append(chars[x]);
+		}
+
+		string result = builder.ToString();
+		return ShuffleString(result);
+	}
+
+	public static string ShuffleString(string value)
+	{
+		return new string(value.OrderBy(x => _random.Next()).ToArray());
 	}
 }
