667bde18adbd37733f235db2d84e9efd7a8c89bb
Rodrigo Geribola <rodrigogeribola@hotmail.com>
2023-08-19 14:56:29 -0300

    partial commit

--- 04 - Infra/DataStruct/Project.SchoolDepartment.Infra.DataStruct.Data/Mappings/StudentMap.cs
+++ 04 - Infra/DataStruct/Project.SchoolDepartment.Infra.DataStruct.Data/Mappings/StudentMap.cs
@@ -90,10 +90,6 @@ public sealed class StudentMap : IEntityTypeConfiguration<Student>
 		builder
 			.Property(x => x.CourseId)
 			.HasDefaultValue(null);
-
-		builder
-			.Property(x => x.UserId)
-			.HasDefaultValue(null);
 	}
 
 	public static void PreLoadedData(ModelBuilder modelBuilder)
@@ -115,7 +111,6 @@ public sealed class StudentMap : IEntityTypeConfiguration<Student>
 				State = "SP",
 				CourseId = Guid.Parse("a69dda80-aed1-452a-afa7-5c09d4885ba1"),
 				SchoolId = Guid.Parse("43b26e0f-93e9-46ca-a574-c5e0b78c7a3b"),
-				UserId = Guid.Parse("9284F301-155A-4642-A526-BD7B941DDD9A")
 			}
 		};
 
